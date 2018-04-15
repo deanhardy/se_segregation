a11f81cd1a5441517181107f3be88c3b50eb9cde
Dean Hardy <deanhardy@gmail.com>
2018-04-14 19:36:47 -0500

    Matching watersheds to urban area

--- script/nhd_grab.R
+++ script/nhd_grab.R
@@ -2,9 +2,22 @@ rm(list=ls())
 
 library(tidyverse)
 library(tigris)
+library(tmap)
 library(FedData)
 library(sf)
 
 atl <-urban_areas('2010') %>%
   st_as_sf() %>%
   filter(NAME10 == 'Atlanta, GA')
+
+## import watershed data
+## note that PERCENTAGE in 'huc' object equals that in ARC (from ArcGIS)
+huc <- st_read("data/spatial/huc10.shp") %>%
+  st_transform(4269)
+
+tm_shape(atl) + 
+  tm_polygons() + 
+  tm_shape(huc) + 
+  tm_borders() + 
+  tm_shape(filter(huc, PERCENTAGE >= 50)) + 
+  tm_borders(col = 'purple')
