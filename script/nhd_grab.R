7cf902585824e90af419b5cfd4ba0986794cc2ec
Dean Hardy <deanhardy@gmail.com>
2018-04-20 09:28:40 -0400

    added HUC12s

--- script/nhd_grab.R
+++ script/nhd_grab.R
@@ -41,18 +41,18 @@ nhd <- rbind(nhd4, nhd3) %>%
                +units=m +no_defs")
 
 ## returns all watersheds that intersect urban area via indexing
-huc <- nhd[atl,]
+huc10 <- nhd[atl,]
 
 ## calculate area & percent of each huc in ATL urban area
-int <- as.tibble(st_intersection(atl, huc))
+int <- as.tibble(st_intersection(atl, huc10))
 
-atl_huc <- int %>%
+atl_huc10 <- int %>%
   mutate(SqKmATLinHUC = as.numeric((st_area(geometry) / 1e6))) %>% 
   mutate(PercATLinHUC = (SqKmATLinHUC/AreaSqKm)) %>%
   select(Name, SqKmATLinHUC, PercATLinHUC) %>%
-  left_join(huc, ., by = 'Name')
+  left_join(huc10, ., by = 'Name')
 
-st_write(atl_huc, "data/data_share/huc10_atlurban", driver = 'GeoJSON')
+st_write(atl_huc10, "data/data_share/huc10_atlurban", driver = 'GeoJSON')
 
 ## ancillary data for mapping reference
 ## grab roads for cartographic purposes
@@ -60,22 +60,22 @@ rd <- primary_roads(year = 2016)
 
 ## plot yea things
 fig <-   
-  tm_shape(atl_huc) + 
+  tm_shape(atl_huc10) + 
     tm_borders(col = "black") + 
   tm_shape(atl) + 
     tm_fill('NAME10', palette = 'grey90',
             title = 'Urban area') +
   tm_shape(rd) +
     tm_lines(col = "black") +
-  tm_shape(atl_huc) + 
+  tm_shape(atl_huc10) + 
     tm_borders(col = "grey50") + 
   tm_shape(arc) +
     tm_borders(lwd = 2) +
   # tm_text('NAME') +
-  tm_shape(filter(atl_huc, PercATLinHUC >= 0.3)) + 
+  tm_shape(filter(atl_huc10, PercATLinHUC >= 0.3)) + 
     tm_borders(col = "red") + 
     tm_text('Name', size = 0.4, col = "black") + 
-  tm_shape(filter(atl_huc, PercATLinHUC >= 0.5)) + 
+  tm_shape(filter(atl_huc10, PercATLinHUC >= 0.5)) + 
     tm_borders(col = "blue") + 
   tm_compass(type = "arrow", size = 2, position = c(0.74, 0.1)) +
   tm_scale_bar(breaks = c(0,20), size = 1, position= c(0.7, 0.0)) +
@@ -92,3 +92,31 @@ tiff('figures/atl_urban_huc10s.tif', res = 300, compression = 'lzw',
 fig
 dev.off()
 
+
+#####################
+## HUC12s processing
+#####################
+
+## import watershed data based on manual extraction
+nhd1 <- st_read("data/spatial/nhd/nhd0307_huc12.shp") 
+nhd2 <- st_read("data/spatial/nhd/nhd0313_huc12.shp")
+nhd3 <- st_read("data/spatial/nhd/nhd0315_huc12.shp")
+nhd4 <- rbind(nhd1, nhd2)
+nhd <- rbind(nhd4, nhd3) %>%
+  st_transform(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 
+               +lon_0=-84 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 
+               +units=m +no_defs")
+
+## returns all watersheds that intersect urban area via indexing
+huc12 <- nhd[atl,]
+
+## calculate area & percent of each huc in ATL urban area
+int <- as.tibble(st_intersection(atl, huc12))
+
+atl_huc12 <- int %>%
+  mutate(SqKmATLinHUC = as.numeric((st_area(geometry) / 1e6))) %>% 
+  mutate(PercATLinHUC = (SqKmATLinHUC/AreaSqKm)) %>%
+  select(Name, SqKmATLinHUC, PercATLinHUC) %>%
+  left_join(huc12, ., by = 'Name')
+
+st_write(atl_huc12, "data/data_share/huc12_atlurban", driver = 'GeoJSON')
